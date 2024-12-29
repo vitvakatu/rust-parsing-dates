038223b6109243cfbc125552210ad1cc2e5ec4ca
Ilya Bogdanov <fumlead@gmail.com>
2024-12-30 00:46:16 +0400

    More test cases

--- src/main.rs
+++ src/main.rs
@@ -64,11 +64,18 @@ fn parse_date_bad(raw: &str) -> Option<Date> {
 }
 
 fn main() {
-    let dates = ["2024-02-31", "64000-6-15"];
+    let dates = [
+        "2024-02-31",
+        "64000-6-15",
+        "01985-04-05",
+        "",
+        "3615-05",
+        "1973-2-3",
+    ];
     for date in dates {
         let date_good = parse_date_good(date);
         let date_bad = parse_date_bad(date);
-        println!("Date: {}", date);
+        println!("Date: {:?}", date);
         println!("\t Good parsing: {:?}", date_good);
         println!("\t Bad parsing: {:?}", date_bad);
     }
